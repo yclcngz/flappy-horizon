44042bf8817bf0c87dc4366fef4d408899990be7
Yucel <yclcngz07@gmail.com>
2026-08-26 00:56:16 +0300

    Add 2nd grade math questions

--- js/questions.js
+++ js/questions.js
@@ -52,6 +52,62 @@ window.questionBank = [
         questionText: "2, 4, 6, ? sıradaki sayı kaçtır?",
         options: ["7", "8", "9", "10"],
         correctAnswerIndex: 1
+    },
+    
+    // --- 2. SINIF MATEMATİK SORULARI (TR) ---
+    {
+        id: "TR_2_001",
+        country: "TR",
+        grade: "2",
+        topic: "Toplama",
+        questionText: "15 + 12 işleminin sonucu kaçtır?",
+        options: ["25", "27", "29", "30"],
+        correctAnswerIndex: 1
+    },
+    {
+        id: "TR_2_002",
+        country: "TR",
+        grade: "2",
+        topic: "Çıkarma",
+        questionText: "30 sayısından 14 çıkarırsak kaç kalır?",
+        options: ["14", "15", "16", "18"],
+        correctAnswerIndex: 2
+    },
+    {
+        id: "TR_2_003",
+        country: "TR",
+        grade: "2",
+        topic: "Çarpma (Temel)",
+        questionText: "Günde 2 elma yiyen Ayşe, 4 günde kaç elma yer? (2 x 4)",
+        options: ["6", "8", "10", "12"],
+        correctAnswerIndex: 1
+    },
+    {
+        id: "TR_2_004",
+        country: "TR",
+        grade: "2",
+        topic: "Uzunluk Ölçüleri",
+        questionText: "1 metre kaç santimetredir?",
+        options: ["10 cm", "50 cm", "100 cm", "1000 cm"],
+        correctAnswerIndex: 2
+    },
+    {
+        id: "TR_2_005",
+        country: "TR",
+        grade: "2",
+        topic: "Zaman",
+        questionText: "Yarım saat kaç dakikadır?",
+        options: ["15", "30", "45", "60"],
+        correctAnswerIndex: 1
+    },
+    {
+        id: "TR_2_006",
+        country: "TR",
+        grade: "2",
+        topic: "Problemler",
+        questionText: "Otobüste 45 yolcu vardı. Durakta 12 kişi indi, 5 kişi bindi. Otobüste kaç yolcu oldu?",
+        options: ["33", "38", "40", "43"],
+        correctAnswerIndex: 1
     }
 ];
 
